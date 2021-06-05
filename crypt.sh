826aefd792d0ca38fcafc84bdd45c466beeb5868
ahmadihamid <ahmadihamid@users.noreply.github.com>
2021-06-06 05:48:17 +0700

    Update crypt.sh

--- crypt.sh
+++ crypt.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-npm install -g https://github.com/robinmoisson/staticrypt
+npm install -g https://github.com/Embraser01/staticrypt
 
 find ./public -type f -name "*.html" -exec staticrypt -i "kecapakses" {} kecapakses -o {} \;
